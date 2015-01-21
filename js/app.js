68233cd16c96f7e6b14c2188a20c5be4809103f2
Ricardo Quintas <rqcenter-web@example.com>
2015-01-21 15:03:22 +0000

    build

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 $(function() {
-	//adjust the header background image
+    var _img1 = true;
+    //adjust the header background image
     $("#ls-header").backstretch("http://placehold.it/3500x1500", {
         speed: 150
     });
+
+
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        console.log(height);
+        if (height > 150) {
+            if (_img1) {
+            	_img1 = false;
+                $('.ls-img').transition({
+                    animation: 'fade up',
+                    duration: '2000ms'
+                });
+            };
+        }
+    });
+
 });
