b01a9329615c70e9ccdafe7e5318d583dc98004b
Ricardo Quintas <rqcenter-web@yahoo.com>
2015-01-23 18:46:20 +0000

    build

--- js/app.js
+++ js/app.js
@@ -3,22 +3,24 @@ $(function() {
 
     //adjust the header background image
     // $("#ls-header").backstretch("http://placehold.it/3500x1500", {
-    $("#ls-header").backstretch("img/header-large.jpg", {
-        speed: 150
-    });
+    // $("#ls-header").backstretch("img/header-large.jpg", {
+    //     speed: 150
+    // });
 
 
-    $(window).scroll(function() {
-        var height = $(window).scrollTop();
-        console.log(height);
-        if (height > 150) {
-            $('#img1').addClass("ls-animated fadeInRight");
-        }
-        if (height > 350) {
-            $('#img2').addClass("ls-animated fadeInLeft");
-        }
-        if (height > 550) {
-            $('#img3').addClass("ls-animated fadeInUp");
-        }
-    });
+
+});
+
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    // console.log(height);
+    if (height > 150) {
+        $('#img1').addClass("ls-animated fadeInRight");
+    }
+    if (height > 350) {
+        $('#img2').addClass("ls-animated fadeInLeft");
+    }
+    if (height > 550) {
+        $('#img3').addClass("ls-animated fadeInUp");
+    }
 });
