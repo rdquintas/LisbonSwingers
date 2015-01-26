4ff1d738ac95142e548dbcdcfec4d9b685cdf6b7
Ricardo Quintas <rqcenter-web@yahoo.com>
2015-01-26 16:37:54 +0000

    build

--- js/app.js
+++ js/app.js
@@ -1,26 +1,47 @@
 $(function() {
-    var _img1 = true;
 
-    //adjust the header background image
-    // $("#ls-header").backstretch("http://placehold.it/3500x1500", {
-    // $("#ls-header").backstretch("img/header-large.jpg", {
-    //     speed: 150
-    // });
+    // Scroll-tos
+    $('.ls-link-menu-1').on("click", function(e) {
+        e.preventDefault();
+        $('#ls-link-menu-1').ScrollTo({
+            duration: 750,
+            easing: 'linear'
+        });
+    });
 
+    $('.ls-link-menu-2').on("click", function(e) {
+        e.preventDefault();
+        $('#ls-link-menu-2').ScrollTo({
+            duration: 1000,
+            easing: 'linear'
+        });
+    });
 
+    // Poppups
+    $('.ls-item-guy').popup();
 
-});
+    // Animations (waypoints)
+    var wpImg1 = new Waypoint({
+        element: $('#img1'),
+        handler: function(direction) {
+            $('#img1').addClass("ls-animated fadeInRight");
+        },
+        offset: "85%"
+    });
 
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    // console.log(height);
-    if (height > 150) {
-        $('#img1').addClass("ls-animated fadeInRight");
-    }
-    if (height > 350) {
-        $('#img2').addClass("ls-animated fadeInLeft");
-    }
-    if (height > 550) {
-        $('#img3').addClass("ls-animated fadeInUp");
-    }
+    var wpImg2 = new Waypoint({
+        element: $('#img2'),
+        handler: function(direction) {
+            $('#img2').addClass("ls-animated fadeInLeft");
+        },
+        offset: "85%"
+    });
+
+    var wpImg3 = new Waypoint({
+        element: $('#img3'),
+        handler: function(direction) {
+            $('#img3').addClass("ls-animated fadeInUp");
+        },
+        offset: "85%"
+    });
 });
